786fef4edd1054d3231f4228b457517b79b4d3bc
Fitri R Dewi <fitri@softwareseni.com>
2025-02-25 16:12:12 +0700

    use global heading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/home.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Eid Mubarak</title>
+        <meta
+          name='description'
+          content='Celebrate Eid with a magical touch!'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/moon.ico' type='image/x-icon' />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import imgSrc from '../assets/images/mountain.png';
 import { Cinzel_Decorative } from 'next/font/google';
@@ -16,7 +15,7 @@ export default function Home() {
     const name = document.getElementById('name') as HTMLInputElement;
     if (name?.value) {
       sessionStorage.setItem('nama', name?.value);
-      window.location.href = '/playground/message.html';
+      window.location.href = 'message';
     }
   };
 
@@ -31,62 +30,49 @@ export default function Home() {
     const scene = document?.getElementById('scene');
     if (scene) new Parallax(scene);
   }, []);
-  
+
   return (
-    <>
-      <Head>
-        <title>Eid Mubarak</title>
-        <meta
-          name='description'
-          content='Celebrate Eid with a magical touch!'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <div>
-        <main className={cinzelDecorative.variable}>
-          <section>
-            <div className='container'>
-              <div id='scene'>
-                <div className='layer'>
-                  <div id='stars'></div>
-                </div>
-                <div className='layer' data-depth-x='-0.15' data-depth-y='0.25'>
-                  <div className='moon'></div>
-                </div>
-                <div className='layer' data-depth-x='0.15'>
-                  <Image
-                    className='mountain'
-                    src={imgSrc}
-                    alt='mountain'
-                    priority
-                  />
-                </div>
-              </div>
+    <main className={cinzelDecorative.variable}>
+      <section>
+        <div className='container'>
+          <div id='scene'>
+            <div className='layer'>
+              <div id='stars'></div>
             </div>
-            <div className='login'>
-              <h2>Eid Mubarak</h2>
-              <p>May your Eid will be full of love, laughter, and light</p>
-              <div className='input-box'>
-                <input
-                  type='text'
-                  placeholder='Your name'
-                  id='name'
-                  onKeyUp={(e) => handleEnter(e)}
-                />
-              </div>
-              <div className='input-box'>
-                <input
-                  type='submit'
-                  id='btn'
-                  value='Open Special Message'
-                  onClick={openMessage}
-                />
-              </div>
+            <div className='layer' data-depth-x='-0.15' data-depth-y='0.25'>
+              <div className='moon'></div>
             </div>
-          </section>
-        </main>
-      </div>
-    </>
+            <div className='layer' data-depth-x='0.15'>
+              <Image
+                className='mountain'
+                src={imgSrc}
+                alt='mountain'
+                priority
+              />
+            </div>
+          </div>
+        </div>
+        <div className='login'>
+          <h2>Eid Mubarak</h2>
+          <p>May your Eid will be full of love, laughter, and light</p>
+          <div className='input-box'>
+            <input
+              type='text'
+              placeholder='Your name'
+              id='name'
+              onKeyUp={(e) => handleEnter(e)}
+            />
+          </div>
+          <div className='input-box'>
+            <input
+              type='submit'
+              id='btn'
+              value='Open Special Message'
+              onClick={openMessage}
+            />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
