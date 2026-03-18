b1c431b2ebc32c8d9d48d082d1c6169701965d14
Fitri Ratna Dewi <fratnadewi@gmail.com>
2026-03-18 22:17:44 +0700

    styling: add several theme colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,34 @@ import '@/styles/home.css';
 import '@/styles/message.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const themeKey = 'theme';
+    const availableThemes = [
+      'theme-purple',
+      'theme-teal',
+      'theme-gold',
+      'theme-rose',
+    ];
+
+    let theme = window.sessionStorage.getItem(themeKey);
+
+    if (!theme || !availableThemes.includes(theme)) {
+      const randomIndex = Math.floor(Math.random() * availableThemes.length);
+      theme = availableThemes[randomIndex];
+      window.sessionStorage.setItem(themeKey, theme);
+    }
+
+    document.body.classList.remove(...availableThemes);
+    if (theme) {
+      document.body.classList.add(theme);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/moon.ico' type='image/x-icon' />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
